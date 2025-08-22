41c5ed38ae66c9e99e1696bfb2f273b4b7d01789
hwanggyuun <gka365@naver.com>
2025-08-22 14:05:17 +0900

    [setting] theme 세팅

---
+++ src/shared/styles/theme.css.ts
@@ -0,0 +1,21 @@
+import { createGlobalTheme } from "@vanilla-extract/css";
+
+export const vars = createGlobalTheme(":root", {
+  color: {
+    KU_Darkgreen: "#036B3F",
+    White: "#FFFFFF",
+    Black: "#000000",
+    Red: "#F44336",
+    Yellow: "#FFAB00",
+    Blue: "#2684FF",
+
+    grey100: "#F8F8F9",
+    grey200: "#EBECEF",
+    grey300: "#DEE0E6",
+    grey400: "#C0C3C9",
+    grey500: "#9B9CA1",
+    grey600: "#5F5F67",
+    grey700: "#3A3C40",
+    grey800: "#151516",
+  },
+});
