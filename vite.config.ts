1a5f754bb46a91c2662897bea36f3dbc5039bd94
jstar_00 <wltjd6300@naver.com>
2025-08-22 14:22:03 +0900

    setting: vite-tsconfig-paths 추가 및 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { fileURLToPath } from "node:url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@api": path.resolve(__dirname, "./src/api"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@shared": path.resolve(__dirname, "./src/shared"),
-      "@components": path.resolve(__dirname, "./src/shared/components"),
-      "@constant": path.resolve(__dirname, "./src/shared/constant"),
-      "@hooks": path.resolve(__dirname, "./src/shared/hooks"),
-      "@styles": path.resolve(__dirname, "./src/shared/styles"),
-      "@types": path.resolve(__dirname, "./src/shared/types"),
-      "@utils": path.resolve(__dirname, "./src/shared/utils"),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 });
