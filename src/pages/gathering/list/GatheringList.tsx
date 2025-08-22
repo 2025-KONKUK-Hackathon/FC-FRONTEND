c96118c0b57b80aeddc56042fa0e7f55be63c2c4
hwanggyuun <gka365@naver.com>
2025-08-22 16:38:39 +0900

    fix: rfc 추가

---
+++ src/pages/gathering/list/GatheringList.tsx
@@ -0,0 +1,5 @@
+export default function GatheringList() {
+  return (
+    <div>GatheringList</div>
+  )
+}
---
+++ src/pages/gathering/members/ApplicantList.tsx
@@ -0,0 +1,5 @@
+export default function ApplicantList() {
+  return (
+    <div>ApplicantList</div>
+  )
+}
