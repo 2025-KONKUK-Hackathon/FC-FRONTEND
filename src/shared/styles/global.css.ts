5da7a972bc9571c42f43f56591bbec5c30c86750
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-08-22 15:35:05 +0900

    feat: global css

---
+++ src/shared/styles/global.css.ts
@@ -0,0 +1,28 @@
+import { globalStyle } from "@vanilla-extract/css";
+import "@styles/layer.css";
+import "@styles/reset.css";
+
+globalStyle("html", {
+  fontSize: "62.5%",
+  scrollPaddingTop: "7rem",
+});
+
+globalStyle("body", {
+  minHeight: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  fontFamily: `'Pretendard', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif`,
+});
+
+globalStyle("#root", {
+  display: "flex",
+  flexDirection: "column",
+  flexGrow: 1,
+});
+
+globalStyle("main", {
+  //TODO: header, footer 추가 후 minHeight 및 paddingTop 수정
+  minHeight: "100vh",
+  flexGrow: 1,
+  paddingTop: "17.16rem",
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    vanillaExtractPlugin({
+      identifiers: "debug",
+    }),
+    visualizer({
+      open: true,
+      filename: "dist/stats.html",
+    }),
+    tsconfigPaths(),
+  ],
+});
