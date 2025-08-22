617433517ee1282b85ef684408a9d459b37f9465
hwanggyuun <gka365@naver.com>
2025-08-22 16:39:17 +0900

    setting: router 설정

---
+++ src/router/constant/Routes.ts
@@ -0,0 +1,17 @@
+export const ROUTES = {
+  AUTH: {
+    LOGIN: "/login",
+    SIGNUP: "/signup",
+  },
+  GATHERING: {
+    CREATE: "/gather/create",
+    DETAIL: "/gather/detail/:id",
+    LIST: "/gather/list",
+    MEMBERS: "/gather/members",
+  },
+  POSTS: {
+    CREATE: "/posts/create",
+    DETAIL: "/posts/detail/:id",
+    LIST: "/posts/list",
+  },
+} as const;
