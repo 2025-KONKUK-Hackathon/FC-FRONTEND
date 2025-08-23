7808b08b238e5abd745c5a96f870953eed1e03df
hwanggyuun <gka365@naver.com>
2025-08-23 17:46:13 +0900

    [fix] scroll 구현

--- src/shared/components/imageBtn/ImageBtn.css.ts
+++ src/shared/components/imageBtn/ImageBtn.css.ts
@@ -2,21 +2,33 @@ import { vars } from '@shared/styles/theme.css';
 import { style } from '@vanilla-extract/css';
 
 export const container = style({
+  width: '100%',
   display: 'flex',
   justifyContent: 'flex-start',
   gap: '0.5rem',
+  overflowX: 'auto',
+  scrollbarWidth: 'none',
+  selectors: {
+    '&::-webkit-scrollbar': {
+      display: 'none',
+    },
+  },
 });
 
 export const imageBtn = style({
-  width: '7rem',
-  aspectRatio: '1/1',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
+
+  width: '7rem',
+  aspectRatio: '1/1',
+  flexShrink: 0,
+
   border: `1.5px dashed ${vars.color.grey400}`,
   borderRadius: '4px',
   backgroundColor: vars.color.grey700,
+
   cursor: 'pointer',
 });
 
@@ -26,9 +38,9 @@ export const image = style({
 });
 
 export const text = style({
-  color: vars.color.grey400,
-  fontSize: '1rem',
   textAlign: 'center',
+  fontSize: '1rem',
+  color: vars.color.grey400,
   padding: '0.5rem 1rem',
 });
 
