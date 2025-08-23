a5e41318d7fb9b377d5f52e6e4a1bb54fad35e9e
hwanggyuun <gka365@naver.com>
2025-08-23 17:30:14 +0900

    add: svg 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,3 +1,4 @@
 // Arrow icons
-export { default as Ic_eye } from "@svg/ic_eye.svg?react";
-export { default as Ic_eye_close } from "@svg/ic_eye_close.svg?react";
+export { default as Ic_eye } from '@svg/ic_eye.svg?react';
+export { default as Ic_eye_close } from '@svg/ic_eye_close.svg?react';
+export { default as Ic_camera } from '@svg/ic_camera.svg?react';
