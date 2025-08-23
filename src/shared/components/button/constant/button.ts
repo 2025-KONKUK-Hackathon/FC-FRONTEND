7e715e4c0fb0962cd907ffc1041aceab508c0672
junghyungo <pp90971658@gmail.com>
2025-08-23 16:46:32 +0900

    feat: button 컴포넌트 구현

---
+++ src/shared/components/button/constant/button.ts
@@ -0,0 +1,12 @@
+export const BUTTON_VARIANT = {
+  FILL: 'fill',
+  OUTLINE: 'outline',
+} as const;
+
+export const BUTTON_SIZE = {
+  SMALL: 'small',
+  LARGE: 'large',
+} as const;
+
+export type ButtonVariant = typeof BUTTON_VARIANT[keyof typeof BUTTON_VARIANT];
+export type ButtonSize = typeof BUTTON_SIZE[keyof typeof BUTTON_SIZE];
