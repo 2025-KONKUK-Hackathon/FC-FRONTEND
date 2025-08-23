b538bdf23b7b4eba1c70d25323e6918c738fcfda
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-08-23 14:58:52 +0900

    feat: 카테고리 상수화

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,108 @@
 import "@styles/global.css.ts";
+import Category from "@shared/components/category/Category";
+import { CLASS_CATEGORY } from "@shared/constant/class";
+import { PART_CATEGORY } from "@shared/constant/part";
+import { GRADE_CATEGORY } from "@shared/constant/grade";
+import { SUBJECT_CATEGORY } from "@shared/constant/subject";
 
 function App() {
   return (
-    <div>
-      <h1>CI-CD</h1>
-      <h2>파이프라인</h2>
-      <h3>테스트</h3>
+    <div style={{ display: "flex", flexWrap: "wrap", gap: "1rem" }}>
+      <Category
+        text={CLASS_CATEGORY.STUDY.text}
+        icon={CLASS_CATEGORY.STUDY.icon}
+        color={CLASS_CATEGORY.STUDY.color}
+      />
+      <Category
+        text={CLASS_CATEGORY.PROJECT.text}
+        icon={CLASS_CATEGORY.PROJECT.icon}
+        color={CLASS_CATEGORY.PROJECT.color}
+      />
+      <Category
+        text={CLASS_CATEGORY.EVENT.text}
+        icon={CLASS_CATEGORY.EVENT.icon}
+        color={CLASS_CATEGORY.EVENT.color}
+      />
+      <Category
+        text={CLASS_CATEGORY.FRIENDSHIP.text}
+        icon={CLASS_CATEGORY.FRIENDSHIP.icon}
+        color={CLASS_CATEGORY.FRIENDSHIP.color}
+      />
+      <Category
+        text={CLASS_CATEGORY.ETC.text}
+        icon={CLASS_CATEGORY.ETC.icon}
+        color={CLASS_CATEGORY.ETC.color}
+      />
+      <Category
+        text={PART_CATEGORY.WEB.text}
+        icon={PART_CATEGORY.WEB.icon}
+        color={PART_CATEGORY.WEB.color}
+      />
+      <Category
+        text={PART_CATEGORY.SERVER.text}
+        icon={PART_CATEGORY.SERVER.icon}
+        color={PART_CATEGORY.SERVER.color}
+      />
+      <Category
+        text={PART_CATEGORY.ETC.text}
+        icon={PART_CATEGORY.ETC.icon}
+        color={PART_CATEGORY.ETC.color}
+      />
+      <Category
+        text={GRADE_CATEGORY.FIRST.text}
+        icon={GRADE_CATEGORY.FIRST.icon}
+        color={GRADE_CATEGORY.FIRST.color}
+      />
+      <Category
+        text={SUBJECT_CATEGORY.COURSE_REGISTRATION.text}
+        icon={SUBJECT_CATEGORY.COURSE_REGISTRATION.icon}
+        color={SUBJECT_CATEGORY.COURSE_REGISTRATION.color}
+      />
+      <Category
+        text={SUBJECT_CATEGORY.CLASS.text}
+        icon={SUBJECT_CATEGORY.CLASS.icon}
+        color={SUBJECT_CATEGORY.CLASS.color}
+      />
+      <Category
+        text={SUBJECT_CATEGORY.TEAM_PROJECT.text}
+        icon={SUBJECT_CATEGORY.TEAM_PROJECT.icon}
+        color={SUBJECT_CATEGORY.TEAM_PROJECT.color}
+      />
+      <Category
+        text={SUBJECT_CATEGORY.ARTICLE.text}
+        icon={SUBJECT_CATEGORY.ARTICLE.icon}
+        color={SUBJECT_CATEGORY.ARTICLE.color}
+      />
+      <Category
+        text={SUBJECT_CATEGORY.CAREER.text}
+        icon={SUBJECT_CATEGORY.CAREER.icon}
+        color={SUBJECT_CATEGORY.CAREER.color}
+      />
+      <Category
+        text={SUBJECT_CATEGORY.PROJECT.text}
+        icon={SUBJECT_CATEGORY.PROJECT.icon}
+        color={SUBJECT_CATEGORY.PROJECT.color}
+      />
+      <Category
+        text={SUBJECT_CATEGORY.STUDY.text}
+        icon={SUBJECT_CATEGORY.STUDY.icon}
+        color={SUBJECT_CATEGORY.STUDY.color}
+      />
+      <Category
+        text={SUBJECT_CATEGORY.INTERNSHIP.text}
+        icon={SUBJECT_CATEGORY.INTERNSHIP.icon}
+        color={SUBJECT_CATEGORY.INTERNSHIP.color}
+      />
+      <Category
+        text={SUBJECT_CATEGORY.ETC.text}
+        icon={SUBJECT_CATEGORY.ETC.icon}
+        color={SUBJECT_CATEGORY.ETC.color}
+      />
+      <Category
+        text={CLASS_CATEGORY.ANNOUCEMENT.text}
+        icon={CLASS_CATEGORY.ANNOUCEMENT.icon}
+        color={CLASS_CATEGORY.ANNOUCEMENT.color}
+      />
     </div>
   );
 }
---
+++ src/shared/constant/subject.ts
@@ -0,0 +1,11 @@
+export const SUBJECT_CATEGORY = {
+  COURSE_REGISTRATION: { text: "수강신청", icon: "🔥", color: "Ocean" },
+  CLASS: { text: "수업", icon: "💡", color: "Charcoal" },
+  TEAM_PROJECT: { text: "팀프로젝트", icon: "🤝", color: "Pink" },
+  ARTICLE: { text: "아티클", icon: "📝", color: "Green" },
+  CAREER: { text: "취업", icon: "🎖️", color: "Orange" },
+  PROJECT: { text: "프로젝트", icon: "💻", color: "Blue" },
+  STUDY: { text: "스터디", icon: "📚", color: "Purple" },
+  INTERNSHIP: { text: "현장실습", icon: "💼", color: "Yellow" },
+  ETC: { text: "기타", icon: "🌈", color: "White" },
+} as const;
