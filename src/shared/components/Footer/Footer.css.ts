ea3600b32758578d9be7517a11944f34d220accf
jstar_00 <wltjd6300@naver.com>
2025-08-23 15:18:07 +0900

    feat: 푸터 디자인 수정

--- src/shared/components/Footer/Footer.css.ts
+++ src/shared/components/Footer/Footer.css.ts
@@ -1,20 +1,29 @@
-import { style } from '@vanilla-extract/css';
-import { vars } from '@styles/theme.css'
+import { style } from "@vanilla-extract/css";
+import { vars } from "@styles/theme.css";
 
 export const container = style({
-    display: 'flex',
-    width: '100%',
-    height: '6rem',
-    backgroundColor: vars.color.grey100,
+  display: "flex",
+  width: "100%",
+  height: "6rem",
+  backgroundColor: vars.color.grey100,
+  position: "fixed",
+  bottom: 0,
+  left: 0,
+  zIndex: 1000,
+  //   boxShadow: "0 -2px 12px rgba(0, 0, 0, 0.08)",
 });
 
 export const navItem = style({
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'column',
-    gap: '0.4rem',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: vars.color.grey500,
-    fontSize: '1.3rem',
-})
+  display: "flex",
+  flex: 1,
+  flexDirection: "column",
+  gap: "0.6rem",
+  alignItems: "center",
+  justifyContent: "center",
+  color: vars.color.grey500,
+  fontSize: "1.4rem",
+});
+
+export const navText = style({
+  color: vars.color.grey900,
+});
--- src/shared/components/Footer/Footer.tsx
+++ src/shared/components/Footer/Footer.tsx
@@ -1,16 +1,20 @@
-import { container, navItem } from "./Footer.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClipboard, faUsers } from "@fortawesome/free-solid-svg-icons";
+import { container, navItem, navText } from "./Footer.css";
 
 const Footer = () => {
   return (
     <div className={container}>
-        <p className={navItem}>
-            <span>게시판</span>
-        </p>
-        <p className={navItem}>
-            <span>모임</span>
-        </p>
+      <button type="button" className={navItem}>
+        <FontAwesomeIcon icon={faClipboard} />
+        <span className={navText}>게시판</span>
+      </button>
+      <button type="button" className={navItem}>
+        <FontAwesomeIcon icon={faUsers} />
+        <span className={navText}>모임</span>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
