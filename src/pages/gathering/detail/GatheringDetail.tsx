ef13fde55c6e96303184ab0c8b8f8f0a348d5240
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-08-24 11:11:54 +0900

    style: size 지정

--- src/pages/gathering/detail/GatheringDetail.tsx
+++ src/pages/gathering/detail/GatheringDetail.tsx
@@ -1,3 +1,9 @@
+import Category from "@/shared/components/category/Category";
+
 export default function GatheringDetail() {
-  return <div>GatheringDetail</div>;
+  return <div style={{ display: "flex", gap: "1rem" ,width:"100%", flexDirection:"column"}}>
+        <Category text="모임간" icon="🕒" color="KU_Darkgreen" size="small" />
+    <Category text="모임 신청기간" icon="🕒" color="KU_Darkgreen" size="medium" />
+    <Category text="모임 활동기간" icon="🕒" color="KU_Darkgreen" size="large" />
+  </div>;
 }
