5c68302d4fa0562422a06394c5811b47fa266246
hwanggyuun <gka365@naver.com>
2025-08-25 03:25:42 +0900

    add: svg 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -11,3 +11,5 @@ export { default as Ic_user } from '@svg/ic_user.svg?react';
 export { default as Ic_calendar } from '@svg/ic_calendar.svg?react';
 export { default as Ic_x } from '@svg/ic_x.svg?react';
 export { default as Ic_personGrey300 } from '@svg/ic_person_grey300.svg?react';
+export { default as Ic_User_Solid } from '@svg/ic_user_solid.svg?react';
+export { default as Ic_User } from '@svg/ic_user.svg?react';
