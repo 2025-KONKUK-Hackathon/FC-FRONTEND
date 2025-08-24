c3027d34e353a7775d4ed23082cf01c7e845c9c7
hwanggyuun <gka365@naver.com>
2025-08-25 03:25:42 +0900

    add: svg 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -2,3 +2,4 @@
 export { default as Ic_eye } from '@svg/ic_eye.svg?react';
 export { default as Ic_eye_close } from '@svg/ic_eye_close.svg?react';
 export { default as Ic_camera } from '@svg/ic_camera.svg?react';
+export { default as Ic_personGrey300 } from '@svg/ic_person_grey300.svg?react';
