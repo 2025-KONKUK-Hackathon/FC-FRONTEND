47d66487764785efe8681a1fd0bb0f9f19104c40
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-08-25 11:47:35 +0900

    feat: 아이콘 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,3 +1,5 @@
 // Arrow icons
-export { default as Ic_eye } from "@svg/ic_eye.svg?react";
-export { default as Ic_eye_close } from "@svg/ic_eye_close.svg?react";
+export { default as Ic_eye } from '@svg/ic_eye.svg?react';
+export { default as Ic_eye_close } from '@svg/ic_eye_close.svg?react';
+export { default as Ic_user } from '@svg/ic_user.svg?react';
+export { default as Ic_calendar } from '@svg/ic_calendar.svg?react';
