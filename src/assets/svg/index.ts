03cb11f4505c56d6096fe8ab119e4bfa920234a6
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-08-25 20:31:58 +0900

    feat: 모임 상세 페이지 개발

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -11,4 +11,3 @@ export { default as Ic_user } from '@svg/ic_user.svg?react';
 export { default as Ic_calendar } from '@svg/ic_calendar.svg?react';
 export { default as Ic_x } from '@svg/ic_x.svg?react';
 export { default as Ic_personGrey300 } from '@svg/ic_person_grey300.svg?react';
-export { default as Ic_User_Solid } from '@svg/ic_user_solid.svg?react';
