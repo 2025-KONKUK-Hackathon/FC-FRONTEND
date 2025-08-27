d9461cc6f5e1078e6375395d66ac399d0774c793
hwanggyuun <gka365@naver.com>
2025-08-27 15:54:38 +0900

    fix: svg 제거

--- src/shared/components/formSection/FormSection.tsx
+++ src/shared/components/formSection/FormSection.tsx
@@ -1,21 +1,17 @@
 import React from "react";
 import * as styles from "@/shared/components/formSection/FormSection.css";
-import { Ic_magnetic, Ic_pin } from "@svg/index";
+
 interface FormSectionProps {
   title: string;
   description: string;
   children: React.ReactNode;
   errorMessage?: string;
-  type: "posts" | "gathering";
 }
 
-export default function FormSection({ title, description, children, errorMessage, type }: FormSectionProps) {
+export default function FormSection({ title, description, children, errorMessage }: FormSectionProps) {
   return (
     <div className={styles.container}>
-      <div className={styles.titleContainer}>
-        {type === "gathering" ? <Ic_pin className={styles.icon} /> : <Ic_magnetic className={styles.icon} />}
         <h2 className={styles.title}>{title}</h2>
-      </div>
       <p className={styles.description}>{description}</p>
       {children}
       {errorMessage && <p className={styles.error}>{errorMessage}</p>}
